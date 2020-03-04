c11f1e20ee52156e31d6e274e29f23f81664bd1d
bizboz1981 <54933192+bizboz1981@users.noreply.github.com>
2020-03-04 15:50:04 +0000

    Create Profile.cs

---
+++ Profile.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace DatingProfile
+{ 
+  class Profile
+  {
+    private string name;
+    private int age;
+    private string city;
+    private string country;
+    private string pronouns;
+    private string[] hobbies;
+    
+    public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+    {
+      {this.name = name;}
+      {this.age = age;}
+      {this.city = city;}
+      {this.country = country;}
+      {this.pronouns = pronouns;}
+      hobbies = new string[0];
+    }
+    
+    public string ViewProfile()
+      {
+        string bio = ($"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}");
+      foreach (string s in this.hobbies)
+      {
+        bio += $"\nHobbies: {s}";
+      }
+      return bio;
+      }
+    
+    public void SetHobbies(string[] hobbies)
+    {
+      this.hobbies = hobbies;
+    }
+  }
+}
